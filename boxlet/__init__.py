9fa21de601ecdfd8008abd4dba8fc0b45a551e32
Werxzy <Werxzyz@yahoo.com>
2022-09-30 21:05:30 -0500

    Disabled opengl imports when sdl2 is the selected render method.

--- boxlet/__init__.py
+++ boxlet/__init__.py
@@ -1,30 +1,37 @@
 import pygame
 import numpy as np
 
-from boxlet.math_extra import *
-import boxlet.opengl.transform_math as Tmath 
+import os
+USE_OPENGL = os.environ.get('BOXLET_RENDER_MODE', 'sdl2') == 'opengl'
 
-from boxlet.opengl.renderer import Renderer
-from boxlet.opengl.render_instance_metaclass import RenderInstance
+from boxlet.math_extra import *
+if USE_OPENGL:
+	import boxlet.opengl.transform_math as Tmath 
+
+	from boxlet.opengl.renderer import Renderer
+	from boxlet.opengl.render_instance_metaclass import RenderInstance
 
 from boxlet.entity import Entity
 from boxlet.manager import instance as manager
 from boxlet.vary_floats import VaryFloats
 
-from boxlet.opengl.model import Model
-from boxlet.opengl.shader import Shader, VertFragShader, ComputeShader
-from boxlet.opengl.texture import Texture
+if USE_OPENGL:
+	from boxlet.opengl.model import Model
+	from boxlet.opengl.shader import Shader, VertFragShader, ComputeShader
+	from boxlet.opengl.texture import Texture
 
-# anything using opengl when imported needs to be imported AFTER pygame is initialized
-# from core.opengl.sprite_renderer import SpriteRenderer
-# from core.opengl.sprite_instanced_renderer import SpriteInstancedRenderer
-from boxlet.opengl.sprite_palette_instanced_renderer import SpritePaletteInstancedRenderer
-from boxlet.opengl.camera_3d import Camera3D
-from boxlet.opengl.model_instanced_renderer import ModelInstancedRenderer
-from boxlet.opengl.render_steps import FrameBufferStep, ApplyShaderToFrame, ApplyDitherToFrame, SimpleClearStep
-from boxlet.opengl.terrain_renderer import TerrainRenderer
+	# anything using opengl when imported needs to be imported AFTER pygame is initialized
+	# from boxlet.opengl.sprite_renderer import SpriteRenderer
+	# from boxlet.opengl.sprite_instanced_renderer import SpriteInstancedRenderer
+	from boxlet.opengl.sprite_palette_instanced_renderer import SpritePaletteInstancedRenderer
+	from boxlet.opengl.camera_3d import Camera3D
+	from boxlet.opengl.model_instanced_renderer import ModelInstancedRenderer
+	from boxlet.opengl.render_steps import FrameBufferStep, ApplyShaderToFrame, ApplyDitherToFrame, SimpleClearStep
+	from boxlet.opengl.terrain_renderer import TerrainRenderer
 
-from boxlet.opengl.camera_controller import CameraController
+	from boxlet.opengl.camera_controller import CameraController
 
 # if build:
 from boxlet.debug import Debug
+
+del USE_OPENGL
