3dd7478631b4242aed1865189ecc212fe254ab6e
Werxzy <Werxzyz@yahoo.com>
2022-10-21 17:20:17 -0500

    Changed UniformBlockObject to instead use offsets in its numpy arrays. Added 2 classes to help with accessing the UniformBlockObject's data.

--- boxlet/opengl/UniformBufferObject.py
+++ boxlet/opengl/UniformBufferObject.py
@@ -26,22 +26,38 @@ c['position'] = [5,5,5,5]
 d = a[0]['data']['direction']
 d[0] = [2,2,2,2]
 
+e = a[0]['data']['position']
+e[3] = [6,3,6,3]
+print(a)
+a[0]['data'][0] = a[0]['data'][1]
+
+print(a)
+
 # asdf = np.dtype([('', vec3),('', 'f4')])
 # print(asdf)
+# print({'names' : ['']} | {'formats' : ['(4,)f4'], 'itemsize' : 16})
+# asdf = np.dtype({'names' : [''], 'formats' : ['(3,)f4'], 'itemsize' : 16})
+# print(asdf)
+# print(np.array([0,0], asdf)[0])
+# asdf = np.dtype({'names' : [''], 'formats' : [vec3], 'itemsize' : 16})
+# # asdf = np.dtype('(4,)f4')
+# print(asdf)
+# print(np.array([0,0], asdf)[0])
+# asdf = np.dtype({
+# 	'names' : ['position', 'color'],
+# 	'formats' : [vec3, vec3],
+# 	'offsets' : [0, 16],
+# 	'itemsize' : 32
+# 	})
 
-asdf = np.dtype([('', vec3),('', 'f4')])
-
-asdf = np.dtype({
-	'names' : ['position', 'color'],
-	'formats' : [vec3, vec3],
-	'offsets' : [0, 16],
-	'itemsize' : 32
-	})
-
-print(asdf)
-test = np.array([0,0], asdf)
-print(test.tobytes())
-print(len(test.tobytes()))
+# print(asdf)
+# test = np.array([0,0], asdf)
+# print(test)
+# print(test[0][0])
+# test[0] = [1,2,3]
+# print(test)
+# print(test.tobytes())
+# print(len(test.tobytes()))
 
 # asdf = np.dtype({
 # 	'names' : ['position','a', 'color','b'],
@@ -83,50 +99,94 @@ DATATYPES = {
 # only going to support mat4, because the gaps on the other array types are too annoying to manage
 
 
+class UBOStructProperty:
+	def __init__(self, data:np.ndarray, base = True) -> None:
+		super().__setattr__('attributes', set())
+		super().__setattr__('other_attrs', {})
+		super().__setattr__('data', data)
+		super().__setattr__('base', base)
+		# self.attributes = set()
+		# self.other_attrs = {}
+		# self.data = data
 
-class UBOProperty:
-	def __init__(self) -> None:
-		...
+		for d in data.dtype.names:
+			next_names = data[d].dtype.names
 
-	def __get__(self, instance, owner):
-		...
-		
-	def __set__(self, instance, value):
-		...
+			if next_names is None:
+				self.attributes.add(d)
+
+			elif '_' in next_names:
+				self.other_attrs[d] = UBOArrayProperty(data[d]['_'][0])
+				print('a',data[d]['_'])
+
+			else:
+				self.other_attrs[d] = UBOStructProperty(data[d], False)
+
+	def __getattr__(self, key):
+		try:
+			if key in self.attributes:
+				if self.base:
+					return self.data[key][0]
+				else:
+					return self.data[key]
+			else:
+				return self.other_attrs[key]
+		except KeyError:
+			return super().__getattribute__(key)
+
+	def __setattr__(self, key, value):
+		try:
+			if key in self.attributes:
+				if self.base:
+					self.data[key][0] = value
+				else:
+					self.data[key] = value
+			else:
+				self.other_attrs[key] = value
+		except KeyError:
+			super().__setattr__(key, value)
+
+	def __str__(self):
+		return str(self.data)
 
 class UBOArrayProperty:
-	def __init__(self) -> None:
-		...
+	def __init__(self, data:np.ndarray) -> None:
+		self._orig_data = data
+		if data[0].dtype.names is None:
+			self.data = data
+		else:
+			self.data = [UBOStructProperty(d, False) for d in data]
 
-	def __get__(self, instance, owner):
-		...
-		
-	def __set__(self, instance, value):
-		...
-# TODO
+	def __getitem__(self, key):
+		return self.data[key]
+
+	def __setitem__ (self, key, item):
+		self.data[key] = item
+
+	def __str__(self):
+		return str(self._orig_data)
+
 
 class UniformBufferObject:
+
 	structure = None
+
 	def __init__(self, structure = None):
 		self.structure = self.structure or structure
-
-		self._data_type,_,_ = self._recursive_check(self.structure)
-
-		self.data = np.zeros(1, self._data_type)
+		self._data_type, _, _ = self._recursive_check(self.structure)
+		self._numpy_data = np.zeros(1, self._data_type)
+		self.data = UBOStructProperty(self._numpy_data)
 
 	@staticmethod
 	def _recursive_check(struct):
-		data_type = []
-		size = 0
-		offset = 0 # just the offset from 0 to 3, the size of vec4 (was planned for something else)
-		alignment = 1
+		names = []
+		formats = []
+		offsets = []
+		item_size = 0
+		alignment = 4
 
-		def fit_whats_left(m, size, offset, data_type:list):
-			if am := (m*4 - offset) % m: 
-				data_type.append(('', f'({am},)f4'))
-				size += am
-				offset = (offset + am) % 4
-			return size, offset
+		def get_needed_offset(align, current_size):
+			return (align*16 - current_size) % align
 
 		for v in struct:
 			if isinstance(v[1], list): # struct
@@ -134,25 +194,27 @@ class UniformBufferObject:
 			else: # value
 				f, s, a = DATATYPES[v[1]]
 
-			l = len(v)
-			if l == 2: # single value
-				size, offset = fit_whats_left(a, size, offset, data_type)
-				data_type.append((v[0], f))
-			elif l == 3: # array
-				size, offset = fit_whats_left(4, size, offset, data_type)
-				a = 4
-				if(s % a != 0):
-					f = np.dtype([('', f),('', f'({a - (s%a)},)f4')])
-				data_type.append((v[0], f, v[2]))
+			if len(v) == 3: # array
+				a = max(16, a)
+				am = get_needed_offset(16, a)
+				a += am
+				s += am
+				s *= s * v[2]
+				f = np.dtype({'names' : ['_'], 'formats' : [np.dtype((f, v[2]))], 'itemsize' : s})
+
+			names.append(v[0]) # append name
+			formats.append(f) # append dtype
 			
-			size += s
-			offset = (offset + s) % 4
+			item_size += get_needed_offset(a, item_size) # gets the offset needed to add to fit the dtype
+			offsets.append(item_size) # append the current offset
+			item_size += s # increase the item size
+			
 			alignment = max(alignment, a)
 
-		size, offset = fit_whats_left(4, size, offset, data_type) 
-		# TODO ??? the instead of 4, maybe use alignment, not sure what a struct of 3 vec2s are suppose to look like yet
+		item_size += get_needed_offset(16, item_size)
+		# TODO ??? the instead of 16, maybe use alignment, not sure what a struct of 3 vec2s is suppose to look like yet
 
-		return np.dtype(data_type), size, alignment
+		return np.dtype({'names' : names, 'formats' : formats, 'offsets' : offsets, 'itemsize' : item_size}), item_size, alignment
 
 
 class LightUniformBuffer(UniformBufferObject):
@@ -165,8 +227,31 @@ class LightUniformBuffer(UniformBufferObject):
 		], 16)
 	]
 
+# How this should be accessed
+# data.light_count = 1
+# l = data.lights[0]
+# l.position = [3,3,3,1]
+# data.lights[0].direction = [1,0,0,0]
+
 f = LightUniformBuffer()
-print(f._data_type)
+print(f.data.light_count)
+print(f.data.lights[0].position)
+f.data.light_count = 1
+l = f.data.lights[0]
+l.position = [2,2,2,1]
+print(f.data.light_count)
+print(f.data.lights[0].position)
+print(f.data.lights)
+# print(f._data_type)
+# print(f.data.tobytes())
+# print(f.data.tobytes())
+# print(f.data.data)
+# print('light_count' in f.data.dtype.names, f.data.dtype.names)
+# print('_' in f.data['lights'].dtype.names, f.data['lights'].dtype.names)
+# print(f.data['lights']['_'].dtype.names)
+# print(f.data['lights']['_']['position'].dtype.names)
+# print(f.data[0].dtype)
+# print(f.data.dtype)
 
 # structobject
 #	list[tuple(contained_type, starting_pos, stride_length)]
